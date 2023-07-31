5484745e63832bc842f819aad63ec87215385ab4
emmancipationera@gmail.com <macbook@Emmanuel-Kalu.local>
2023-07-31 22:38:43 +0100

    update heroimage

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -1,8 +1,8 @@
-
+import heroImage from '../assets/images/image1.jpg'
 const HeroPage = () => {
   return (
   <section className="HeroPage">   
-    <div className="heroimage-container">  
+    <div className="heroimage-container" style={{backgroundImage: `url(${heroImage})`}}>  
       <div className="herotext-container">
         <h1> A Place Of <span>Luxury</span></h1>
         <p>This is the place you want to be for your booking for an apartment.</p>
